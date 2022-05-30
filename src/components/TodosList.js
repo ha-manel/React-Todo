1c8c4e193b760d3d34eb9c2b949577b18adbbdc7
Manel <manelhammouche3@gmail.com>
2022-05-30 16:09:34 +0100

    Add todos list component

---
+++ src/components/TodosList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+const TodosList = () => (
+  <div id="todos-list">
+    <TodoItem />
+    <TodoItem />
+  </div>
+);
+
+export default TodosList;
